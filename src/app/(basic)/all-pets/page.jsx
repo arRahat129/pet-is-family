0fa9c3d311c996d2b7c32edb555b834fac7abf9a
arRahat129 <rahashik129@gmail.com>
2026-05-18 20:54:50 +0600

    created petCard, shown pets data on all pets page, and configured image protocol and hostname in next.config file

--- src/app/(basic)/all-pets/page.jsx
+++ src/app/(basic)/all-pets/page.jsx
@@ -1,9 +1,35 @@
+import PetCard from '@/components/pet/PetCard';
 import React from 'react';
 
-const AllPets = () => {
+const AllPets = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/pet`);
+
+    const allPetData = await res.json();
+    console.log(allPetData);
+
     return (
-        <div className='max-w-7xl mx-auto'>
-            <h1 className='text-2xl font-bold text-center'>All Pets</h1>
+        <div className="max-w-7xl mx-auto px-4 py-10">
+
+            <div className="text-center mb-12">
+                <h1 className="text-3xl md:text-4xl font-bold text-green-800">
+                    Find Your New Best Friend
+                </h1>
+
+                <p className="text-gray-500 mt-3 max-w-2xl mx-auto">
+                    Browse adorable pets waiting for a loving home.
+                </p>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {
+                    allPetData.map((petData) => (
+                        <PetCard
+                            key={petData._id}
+                            petData={petData}
+                        />
+                    ))
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/pet/PetCard.jsx
@@ -0,0 +1,98 @@
+import { Card, Chip, Separator, Button } from "@heroui/react";
+import { DollarSign } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaMars, FaVenus } from "react-icons/fa";
+import { MdPets } from "react-icons/md";
+
+const PetCard = ({ petData }) => {
+    // console.log(petData);
+
+    const {
+        _id,
+        imageUrl,
+        petName,
+        species,
+        breed,
+        gender,
+        adoptionFee,
+    } = petData;
+
+    return (
+        <Card className="group overflow-hidden border shadow-sm hover:shadow-xl transition-all duration-300 rounded-3xl">
+
+            <div className="relative overflow-hidden">
+                <Image
+                    src={imageUrl}
+                    alt={petName}
+                    width={500}
+                    height={500}
+                    className="w-full h-72 object-cover group-hover:scale-105 transition-transform duration-500"
+                />
+
+                <Chip
+                    size="sm"
+                    className="absolute top-4 left-4 bg-white text-black font-medium"
+                >
+                    <div className="flex items-center gap-1">
+                        <MdPets />
+                        {species}
+                    </div>
+                </Chip>
+            </div>
+
+            <div className="p-5 space-y-4">
+
+                <div>
+                    <h1 className="text-2xl font-bold text-gray-800">
+                        {petName}
+                    </h1>
+
+                    <p className="text-gray-500 text-sm mt-1">
+                        {breed}
+                    </p>
+                </div>
+
+                <div className="flex items-center justify-between text-sm text-gray-600">
+
+                    <div className="flex items-center gap-2">
+                        {
+                            gender === "Male"
+                                ? <FaMars className="text-blue-500" />
+                                : <FaVenus className="text-pink-500" />
+                        }
+
+                        <span>{gender}</span>
+                    </div>
+
+                    <div className="flex items-center gap-1 font-semibold text-green-700">
+                        <DollarSign size={20} />
+                        {adoptionFee}
+                    </div>
+                </div>
+
+                <Separator />
+
+                <div className="flex gap-3">
+                    <Link href={`#`}>
+                        <Button
+                            className="w-full bg-green-600 text-white font-medium rounded-xl"
+                        >
+                            View Details
+                        </Button>
+                    </Link>
+
+                    <Button
+                        variant="outline"
+                        className="rounded-xl hover:bg-purple-100"
+                    >
+                        Adopt Now
+                    </Button>
+                </div>
+            </div>
+        </Card>
+    );
+};
+
+export default PetCard;
